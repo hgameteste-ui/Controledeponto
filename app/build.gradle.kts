42fe45bead9d415ca911e354dec41079f4eaaf92
hgameteste <hgame.teste@gmail.com>
2026-07-07 21:31:45 -0300

    Versão 3.2.1 - Mover status de nuvem para Toolbar e adicionar ícone dinâmico

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,9 @@ android {
         // The target Android version for app behavior
         targetSdk = 35
         // Internal version number for updates
-        versionCode = 320
+        versionCode = 321
         // User-visible version name
-        versionName = "3.2.0"
+        versionName = "3.2.1"
 
         // Runner for instrumentation tests
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
