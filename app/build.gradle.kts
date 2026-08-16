db3182afef07a4b89f85daef9569e82f8f1ed6b5
hgameteste <hgame.teste@gmail.com>
2026-08-16 18:13:39 -0300

    release: Versão 3.3.7 - Backup CSV

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,9 @@ android {
         // The target Android version for app behavior
         targetSdk = 35
         // Internal version number for updates
-        versionCode = 336
+        versionCode = 337
         // User-visible version name
-        versionName = "3.3.6"
+        versionName = "3.3.7"
 
         // Runner for instrumentation tests
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
